0cd9b51b80a4b3f40a5bb86cfdbdf9ccef1d6aed
WILLIAM JUNIOR VILORIA QUEZADA <1106615@est.intec.edu.do>
2022-07-17 22:12:41 -0400

    gfg

--- ProyectoFinal/Form1.cs
+++ ProyectoFinal/Form1.cs
@@ -7,14 +7,25 @@ namespace ProyectoFinal
             InitializeComponent();
         }
 
+<<<<<<< HEAD
         private void Form1_Load(object sender, EventArgs e)
+=======
+        private void label1_Click(object sender, EventArgs e)
+>>>>>>> 0dc32e1e06d9e3195faf5095215f9012aa818dba
         {
 
         }
 
+<<<<<<< HEAD
         private void button1_Click(object sender, EventArgs e)
+=======
+        private void textBox1_TextChanged(object sender, EventArgs e)
+>>>>>>> 0dc32e1e06d9e3195faf5095215f9012aa818dba
         {
 
         }
     }
 }
+
+
+// william mmg
